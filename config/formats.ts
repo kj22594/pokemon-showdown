bc6a7814a9dd046d660659b2ef853d98051db4fd
Kj2294 <kstaley22594@gmail.com>
2022-03-04 14:39:11 -0500

    Add random battles back

--- config/formats.ts
+++ config/formats.ts
@@ -48,4 +48,18 @@ export const Formats: FormatList = [
 			'Quick Claw', 'Baton Pass',
 		],
 	},
+	{
+		section: "Other Formats",
+	},
+	{
+		name: "[Gen 8] Random Battle",
+		desc: `Randomized teams of level-balanced Pok&eacute;mon with sets that are generated to be competitively viable.`,
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3656537/">Random Battle Suggestions</a>`,
+		],
+
+		mod: 'gen8',
+		team: 'random',
+		ruleset: ['PotD', 'Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod'],
+	},
 ];
