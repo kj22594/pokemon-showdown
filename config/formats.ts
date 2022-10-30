00bb09a7cc7b2e74be6a52aec396f6f58dd1c6e3
Kj2294 <kstaley22594@gmail.com>
2022-10-30 18:43:15 -0400

    Add ISL format to showdown

--- config/formats.ts
+++ config/formats.ts
@@ -491,7 +491,43 @@ export const Formats: FormatList = [
 
 	// National Dex
 	///////////////////////////////////////////////////////////////////
+    {
+        section: "ISL Formats",
+    },
+    {
+        name: "[Gen 8] Draft",
+        threads: [
+            `&bullet; <a href="https://www.smogon.com/forums/threads/3666135/">National Dex Metagame Discussion</a>`,
+            `&bullet; <a href="https://www.smogon.com/forums/threads/3667921/">National Dex Sample Teams</a>`,
+            `&bullet; <a href="https://www.smogon.com/forums/threads/3666572/">National Dex Viability Rankings</a>`,
+        ],
 
+        mod: 'gen8',
+        ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
+        banlist: [
+            'Arceus', 'Calyrex-Ice', 'Calyrex-Shadow', 'Cinderace + Libero', 'Darkrai',
+            'Deoxys-Attack', 'Deoxys-Base', 'Deoxys-Speed', 'Dialga', 'Dracovish', 'Eternatus',
+            'Genesect', 'Giratina', 'Giratina-Origin', 'Greninja + Protean', 'Groudon', 'Ho-Oh', 'Kyogre', 'Kyurem-Black',
+            'Kyurem-White', 'Landorus-Base + Sheer Force', 'Lugia', 'Lunala', 'Magearna', 'Marshadow', 'Mewtwo',
+            'Naganadel', 'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane', 'Necrozma-Ultra', 'Palkia', 'Pheromosa', 'Rayquaza', 'Reshiram',
+            'Shaymin-Sky', 'Solgaleo', 'Urshifu-Base', 'Xerneas', 'Yveltal', 'Zacian',
+            'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom', 'Zygarde-Base', 'Zygarde-Complete', 'Arena Trap', 'Battle Bond', 'Moody',
+            'Power Construct', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'Bright Powder', 'King\'s Rock', 'Lax Incense', 'Razor Fang',
+            'Quick Claw', 'Baton Pass', 'Buginium Z','Darkinium Z','Dragonium Z','Electrium Z','Fairium Z','Fightinium Z','Firium Z','Flyinium Z',
+            'Ghostium Z','Grassium Z','Icium Z','Normalium Z','Poisonium Z','Psychium Z','Rockium Z','Steelium Z','Waterium Z','Aloraichium Z','Decidium Z','Eevium Z',
+            'Incinium Z','Kommonium Z','Lunalium Z','Lycanium Z','Marshadium Z','Mewnium Z','Mimikium Z','Pikanium Z','Pikashunium Z','Primarium Z','Snorlium Z','Solganium Z',
+            'Tapunium Z','Ultranecrozium Z', 'Abomasnow-Mega', 'Absol-Mega', 'Aerodactyl-Mega', 'Aggron-Mega', 'Alakazam-Mega', 'Altaria-Mega', 'Ampharos-Mega', 'Audino-Mega',
+            'Banette-Mega', 'Beedrill-Mega', 'Blastoise-Mega', 'Blaziken-Mega', 'Camerupt-Mega', 'Charizard-Mega-X', 'Charizard-Mega-Y', 'Diancie-Mega', 'Gallade-Mega', 'Garchomp-Mega', 'Gardevoir-Mega',
+            'Gengar-Mega', 'Glalie-Mega', 'Gyarados-Mega', 'Heracross-Mega', 'Houndoom-Mega', 'Kangaskhan-Mega', 'Latias-Mega', 'Latios-Mega', 'Lopunny-Mega', 'Lucario-Mega', 'Manectric-Mega',
+            'Mawile-Mega', 'Medicham-Mega', 'Metagross-Mega', 'Mewtwo-Mega-X', 'Mewtwo-Mega-Y', 'Pidgeot-Mega', 'Pinsir-Mega', 'Rayquaza-Mega', 'Sableye-Mega', 'Salamence-Mega', 'Sceptile-Mega', 'Scizor-Mega',
+            'Sharpedo-Mega', 'Slowbro-Mega', 'Steelix-Mega', 'Swampert-Mega', 'Tyranitar-Mega', 'Venusaur-Mega', 'Karate Chop', 'Double Slap', 'Comet Punch', 'Razor Wind', 'Jump Kick', 'Rolling Kick',
+            'Twineedle', 'Sonic Boom', 'Dragon Rage', 'Meditate', 'Rage', 'Barrier', 'Bide', 'Mirror Move', 'Egg Bomb', 'Bone Club', 'Clamp', 'Spike Cannon', 'Constrict', 'Kinesis', 'Barrage', 'Bubble',
+            'Dizzy Punch', 'Flash', 'Psywave', 'Sharpen', 'Spider Web', 'Nightmare', 'Feint Attack', 'Foresight', 'Return', 'Frustration', 'Magnitude', 'Pursuit', 'Hidden Power', 'Smelling Salts', 'Assist',
+            'Refresh', 'Snatch', 'Secret Power', 'Camouflage', 'Mud Sport', 'Ice Ball', 'Needle Arm', 'Odor Sleuth', 'Silver Wind', 'Grass Whistle', 'Signal Beam', 'Sky Uppercut', 'Water Sport', 'Miracle Eye',
+            'Wake-Up Slap', 'Natural Gift', 'Embargo', 'Trump Card', 'Heal Block', 'Wring Out', 'Lucky Chant', 'Me First', 'Punishment', 'Mud Bomb', 'Mirror Shot', 'Rock Climb', 'Magnet Bomb', 'Captivate', 'Heal Order',
+            'Ominous Wind', 'Telekinesis', 'Flame Burst', 'Synchronoise', 'Chip Away', 'Sky Drop', 'Bestow', 'Heart Stamp', 'Steamroller', 'Rototiller', 'Ion Deluge',
+        ],
+    },
 	{
 		section: "National Dex",
 	},
